96886b11506d2b92e855fab04ace557379864d88
ahmedalhindi <alhindi.ahmed@gmail.com>
2020-01-06 16:59:42 +0300

    Lifecycle and events

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Lyfecycle from './lyfecycle/Lyfecycle'
+import Header from './header/Header'
+import Memegen from './memegen/Memegen';
 
 function App() {
   return (
     <div className="App">
+      <Lyfecycle />
+      <Header />
+      <Memegen />
     </div>
   );
 }
